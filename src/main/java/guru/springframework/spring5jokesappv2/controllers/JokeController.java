c24a505497dd19d83312bed6dd9de6139c46fee8
leandi <sandi.kurtin@gmail.com>
2021-05-26 08:06:20 +0200

    add template to show joke closes #3.

--- src/main/java/guru/springframework/spring5jokesappv2/controllers/JokeController.java
+++ src/main/java/guru/springframework/spring5jokesappv2/controllers/JokeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class JokeController {
-    private final JokeService jokeService
+    private final JokeService jokeService;
 
     public JokeController(JokeService jokeService) {
         this.jokeService = jokeService;
